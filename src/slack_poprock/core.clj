cee330b78ca3d2781caae22ce95cd59ed6628ad8
Ben Biddington <ben.biddington@gmail.com>
2015-04-10 15:12:54 +1200

    Expanded vocab

--- src/slack_poprock/core.clj
+++ src/slack_poprock/core.clj
@@ -7,8 +7,22 @@
 (require '[aleph.http :as http])
 (require '[clj-http.client :as client])
 
-(def user-name "@U04B4FE2Y")
+(def replies
+  [
+    "Heck Mandy!"
+    "Ma balls!" 
+    "Mate! I've just had a shower!"
+    "Rotten dog! :dog:"
+    "Chuck it on the fire! :fire:"
+    "Who's farting? :boom:"
+    "Crikey dick!"
+    "Goh-lee!"
+    "Mol!"
+    "Col!"
+    "Joss-ette!"])
 
+(def user-name "@U04B4FE2Y")
+(def nick-name "poppo")
 (def token (System/getenv "TOKEN")) ;; => https://trapslinger.slack.com/services/4345477538?icon=1
 (def start-url (str "https://slack.com/api/rtm.start?token=" token))
 
@@ -27,18 +41,25 @@
   (let [all (slack-start-info)]
     (:channels all))) 
 
+(defn- mentioned[text,what]
+  (if (clojure.string/blank? text) false (.contains text what)))
+
 (defn- mentioned-me[msg]
-  (let [text (:text msg)]
-    (if (clojure.string/blank? text) false (.contains text  user-name))))
+  (or (mentioned (:text msg) user-name) (mentioned (:text msg) nick-name)))
+
+(defn- mentioned-chocolate[msg]
+  (mentioned (:text msg) "chocolate"))
 
 (defn- reply-with[channel,text]
-  ;;(streams/put! c "{\"id\": 1, \"type\": \"message\", \"channel\": \"C04AUKBH5\", \"text\": \"Hi everybody\"}") 
   (streams/put! c (json/write-str {:id 1 :type "message" :channel channel :text text}))
   (prn text))
 
 (defn- reply[to]
   (when (mentioned-me to)
-    (reply-with (:channel to) "Heck Mandy!")))
+    (reply-with (:channel to) (rand-nth replies)))
+
+  (when (mentioned-chocolate to)
+    (reply-with (:channel to) "Did someone say chocolate?")))
 
 (defn- listen[message]
   (prn 'message! message)
@@ -54,3 +75,4 @@
     (Thread/sleep 1000)))
 
 ;; Realtime API => https://api.slack.com/rtm
+;; Emoticons -> http://www.emoji-cheat-sheet.com/
