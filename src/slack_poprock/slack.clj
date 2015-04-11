4d494dd8b2818c0684d990380defdba544c07671
Ben Biddington <ben.biddington@gmail.com>
2015-04-11 21:59:47 +1200

    Added `slack/users`

--- src/slack_poprock/slack.clj
+++ src/slack_poprock/slack.clj
@@ -4,11 +4,25 @@
             [aleph.http :as http]
             [clj-http.client :as client]))
 
+(defrecord User [name real-name id title])
+
+(defn- clean[text] (if (clojure.string/blank? text) "" text))
+(defn- to-user [what] 
+  (User. 
+   (clean (:name what)) 
+   (clean (:real_name what)) 
+   (:id what) 
+   (clean (-> what :profile :title))))
+
+(defn- to-users [what] (map to-user what))
+
 (defn- deserialize      [what]  (json/read-str what :key-fn keyword))
+
 (defn- start-url        [token] (str "https://slack.com/api/rtm.start?token=" token))
 (defn- slack-start-info [token] (deserialize (:body (client/get (start-url token)))))
 (defn- slack-start-url  [token] (:url (slack-start-info token)))
 
-(defn settings          [token] (slack-start-info token))
-;;(defn connect-to[url] (aleph.http/websocket-client url))
+(defn- settings         [token] (slack-start-info token))
+(defn users             [token] (to-users (:users (settings token))))
 
+;;(defn connect-to[url] (aleph.http/websocket-client url))
--- test/slack_poprock/core_test.clj
+++ test/slack_poprock/core_test.clj
@@ -4,9 +4,11 @@
             [cheshire.core :refer :all :as c]
             [slack-poprock.slack :refer :all :as slack]))
 
+(defn- json-pretty[what] (c/generate-string what {:pretty true}))
+
 (def ^{:private true} token (System/getenv "TOKEN")) ;; => https://trapslinger.slack.com/services/4345477538?icon=1
 
 (deftest that-you-can-query-slack-for-various-data
-  (testing "List contacts like this, for example"
-    (let [settings (slack/settings token)]
-      (println (c/generate-string settings {:pretty true})))))
+  (testing "List users like this, for example"
+    (let [users (slack/users token)]
+      (is (some #(= "beans" (:name %)) users) "Expected to find a user called \"beans\""))))
