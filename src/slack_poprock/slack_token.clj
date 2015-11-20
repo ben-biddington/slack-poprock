4cb0a7b47e8fc848c8242d6d1256f037f796f9ee
Ben Biddington <ben.biddington@gmail.com>
2015-11-20 20:02:26 +1300

    :green_apple: Fail better when token is missing

--- src/slack_poprock/slack_token.clj
+++ src/slack_poprock/slack_token.clj
@@ -1,6 +1,14 @@
-(ns slack-poprock.slack-token)
+(ns slack-poprock.slack-token
+  (:require [clojure.java.io :as io]))
+  
+(def filename ".token")
+
+(defn- disk[]
+  (when (not (.exists (io/file filename)))
+    (throw (Exception. (format "Expected a file at <%s> to contain slack token. Find that token at <https://trapslinger.slack.com/services/4378524098>." filename))))
+  (slurp filename))
 
 (def current      
   (or 
    (System/getenv "TOKEN") 
-   (slurp ".token"))) ;; => https://trapslinger.slack.com/services/4345477538?icon=1
+   (disk))) ;; => https://trapslinger.slack.com/services/4345477538?icon=1
